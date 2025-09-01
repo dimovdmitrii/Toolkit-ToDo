7cba94d3196ae67c93be50d92033b84a9c7d398d
dimovdmitrii <dimovdima1509@gmail.com>
2025-09-01 11:14:02 +0200

    add todo action

--- src/components/todoForm/index.jsx
+++ src/components/todoForm/index.jsx
@@ -1,13 +1,34 @@
 import styles from "./styles.module.css";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo } from "../../redux/slices/todoSlice";
 
 function TodoForm() {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const newTodo = {
+      id: Math.random(),
+      title: title,
+      completed: false,
+    };
+
+    dispatch(addTodo(newTodo));
+    setTitle("");
+  };
+
   return (
-    <form className={styles.form}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type="text"
         name="todoTitle"
         className={styles.todoTitle}
         placeholder="Enter new todo"
+        value={title}
+        onChange={(event) => setTitle(event.target.value)}
       />
       <input
         type="submit"
